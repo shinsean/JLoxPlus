950d77e31111b47b7ca56b1aedacf6e533ceb23b
shinsean <seunggyu.shin1@gmail.com>
2024-08-06 20:57:12 -0600

    Add methods to interpret var assign in Interpreter

--- src/main/java/org/shinsean/lox/Interpreter.java
+++ src/main/java/org/shinsean/lox/Interpreter.java
@@ -196,4 +196,13 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         environment.define(stmt.name.lexeme, value);
         return null;
     }
+
+    // TODO: Consider position of this method. It does make sense to have it after visitVarStmt,
+    //  but it also makes sense to group it with the other expressions.
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
 }
