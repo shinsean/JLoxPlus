6be9b33716f2501ef816de01c8a46cc5357c47df
shinsean <seunggyu.shin1@gmail.com>
2024-08-06 19:20:19 -0600

    Update Expr and Stmt classes with var declaration

--- src/main/java/org/shinsean/tool/GenerateAst.java
+++ src/main/java/org/shinsean/tool/GenerateAst.java
@@ -15,15 +15,17 @@ public class GenerateAst {
 
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
-            "Binary     : Expr left, Token operator, Expr right",
-            "Grouping   : Expr expression",
-            "Literal    : Object value",
-            "Unary      : Token operator, Expr right"
+                "Binary     : Expr left, Token operator, Expr right",
+                "Grouping   : Expr expression",
+                "Literal    : Object value",
+                "Unary      : Token operator, Expr right",
+                "Variable   : Token name"
         ));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Expression : Expr expression",
-                "Print      : Expr expression"
+                "Print      : Expr expression",
+                "Var        : Token name, Expr initializer"
         ));
     }
 
