97fa4da322e9be716a01b22b7f0b96a2777389e3
shinsean <seunggyu.shin1@gmail.com>
2024-08-10 09:37:29 -0600

    Add support for closures to enable nested funcs
    Note: As of now, this has some dynamic scoping issues, which will be
    addressed shortly.

--- src/main/java/org/shinsean/lox/LoxFunction.java
+++ src/main/java/org/shinsean/lox/LoxFunction.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 public class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment closure) {
         this.declaration = declaration;
+        this.closure = closure;
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             // Get the name of the function scope env variable from the parameter names
             // from the function declaration, and get the corresponding values from the
