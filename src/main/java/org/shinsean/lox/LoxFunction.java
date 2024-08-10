758753acec3edc927c9e643f49b48d4e002a2aa4
shinsean <seunggyu.shin1@gmail.com>
2024-08-10 08:42:47 -0600

    Create class for runtime reprsntation of Lox funcs

---
+++ src/main/java/org/shinsean/lox/LoxFunction.java
@@ -0,0 +1,36 @@
+package org.shinsean.lox;
+
+import java.util.List;
+
+public class LoxFunction implements LoxCallable {
+    private final Stmt.Function declaration;
+
+    LoxFunction(Stmt.Function declaration) {
+        this.declaration = declaration;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        Environment environment = new Environment(interpreter.globals);
+        for (int i = 0; i < declaration.params.size(); i++) {
+            // Get the name of the function scope env variable from the parameter names
+            // from the function declaration, and get the corresponding values from the
+            // passed-in argument variables. This is safe to do because visitCallExpr
+            // checks arity before calling "call()".
+            environment.define(declaration.params.get(i).lexeme, arguments.get(i));
+        }
+
+        interpreter.executeBlock(declaration.body, environment);
+        return null;
+    }
+
+    @Override
+    public int arity() {
+        return declaration.params.size();
+    }
+
+    @Override
+    public String toString() {
+        return "<fn " + declaration.name.lexeme + ">";
+    }
+}
