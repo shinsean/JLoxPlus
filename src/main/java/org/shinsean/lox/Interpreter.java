e4ec195b535300575d837b56ae5e3b9d452f69b3
shinsean <seunggyu.shin1@gmail.com>
2024-08-10 08:55:47 -0600

    Create global env; create clock native function
    The clock native function will return the number of milliseconds that
    has passed since 1 Jan 1970 midnight at the moment of the call according to the
    system clock.

--- src/main/java/org/shinsean/lox/Interpreter.java
+++ src/main/java/org/shinsean/lox/Interpreter.java
@@ -4,7 +4,28 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    Interpreter() {
+        // This defines clock in globals by pairing it with an instance of anonymous Java class.
+        globals.define("clock", new LoxCallable() {
+            @Override
+                public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public int arity() {
+                return 0;
+            }
+
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
