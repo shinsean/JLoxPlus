bc485667bda09d604215455a3ad8f4cc81a161e2
shinsean <seunggyu.shin1@gmail.com>
2024-08-12 23:29:43 -0600

    Generate syntax classes for class inheritance

--- src/main/java/org/shinsean/lox/Expr.java
+++ src/main/java/org/shinsean/lox/Expr.java
@@ -11,6 +11,7 @@ abstract class Expr {
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
         R visitLogicalExpr(Logical expr);
+        R visitSuperExpr(Super expr);
         R visitThisExpr(This expr);
         R visitSetExpr(Set expr);
         R visitUnaryExpr(Unary expr);
@@ -115,6 +116,20 @@ abstract class Expr {
         final Expr left;
         final Token operator;
         final Expr right;
+    }
+    static class Super extends Expr {
+        Super(Token keyword, Token method) {
+            this.keyword = keyword;
+            this.method = method;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitSuperExpr(this);
+        }
+
+        final Token keyword;
+        final Token method;
     }
     static class This extends Expr {
         This(Token keyword) {
